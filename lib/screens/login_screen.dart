f8cc75ece77b64e4c30603f9d282f6e48d74af35
Fábio Luiz Camacho Pacheco <fabiopacheco@iMac-de-Fabio.local>
2020-07-21 15:08:09 -0300

    feat: create reusable text form

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,22 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:yoganath/widgets/reusableTextFormField.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  String _email = '';
+  String _password = '';
+  String _formData = '';
+  bool _checked = false;
+
   @override
   Widget build(BuildContext context) {
-    // TODO Criar Scaffold
-    return Column(
-      children: <Widget>[
-        Hero(
-          tag: 'logo',
-          child: Image.asset(
-            'assets/images/logo.jpg',
-            width: MediaQuery.of(context).size.width * 0.848,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Image.asset(
+                    'assets/images/logo.jpg',
+                    width: MediaQuery.of(context).size.width * 0.848,
+                  ),
+                ),
+                SizedBox(
+                  height: 70.54,
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: ReusableTextFormField(
+                          textEditingController: _emailController,
+                          isObscure: false,
+                          label: 'E-mail',
+                          hint: 'meu@email.com',
+                          errorMessage: 'Por favor, digite seu e-mail',
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: ReusableTextFormField(
+                          textEditingController: _passController,
+                          isObscure: true,
+                          label: 'Senha',
+                          hint: null,
+                          errorMessage: 'Por favor, digite sua senha',
+                          keyboardType: TextInputType.text,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 32.0,
+                      ),
+                      RaisedButton(
+                          child: Text('ENTRAR'),
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              _formKey.currentState.save();
+
+                              setState(() {
+                                _formData =
+                                    'E-mail: $_email; Senha: $_password.';
+                              });
+                              print(_formData);
+                            }
+                          }),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 32.0,
+                ),
+                Text(
+                  'Ainda não é cadastrado?',
+                ),
+                FlatButton(
+                  onPressed: () {},
+                  child: Text('Cadastrar'),
+                ),
+                SizedBox(
+                  height: 70.0,
+                ),
+                CheckboxListTile(
+                    title: Text(
+                        'Ao continuar, você concorda com os Termos de Uso e Política de Privacidade do YogaNath.'),
+                    value: _checked,
+                    onChanged: (bool newValue) {
+                      setState(() {
+                        _checked = newValue;
+                      });
+                    },
+                    activeColor: Theme.of(context).colorScheme.secondaryVariant,
+                    controlAffinity: ListTileControlAffinity.platform),
+              ],
+            ),
           ),
         ),
-        Text('E-mail'),
-        Text('Senha'),
-        Text('Botão')
-      ],
+      ),
     );
   }
 }
---
+++ lib/widgets/reusableTextFormField.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ReusableTextFormField extends StatelessWidget {
+  ReusableTextFormField(
+      {@required this.textEditingController,
+      @required this.isObscure,
+      @required this.label,
+      @required this.hint,
+      @required this.errorMessage,
+      @required this.keyboardType});
+
+  final TextEditingController textEditingController;
+  final bool isObscure;
+  final String label;
+  final String hint;
+  final String errorMessage;
+  final TextInputType keyboardType;
+
+  @override
+  Widget build(BuildContext context) {
+    IconButton showEyeIcon() {
+      if (isObscure) {
+        return IconButton(
+          icon: Icon(Icons.visibility),
+          onPressed: null,
+        );
+      }
+      return null;
+    }
+
+    return TextFormField(
+      keyboardType: keyboardType,
+      controller: textEditingController,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        labelStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
+        filled: true,
+        suffixIcon: showEyeIcon(),
+      ),
+      cursorColor: Theme.of(context).colorScheme.primary,
+      obscureText: isObscure,
+      validator: (typedEmail) {
+        if (typedEmail.isEmpty) {
+          return errorMessage;
+        }
+        return null;
+      },
+    );
+  }
+}
