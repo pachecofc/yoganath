5462842ff991bc486b285e5f679be8200971fcd9
Fábio Luiz Camacho Pacheco <fabiopacheco@iMac-de-Fabio.local>
2020-07-22 08:27:29 -0300

    refactor: create sless widget in main method

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,39 @@ import 'package:yoganath/services/routeGenerator.dart';
 
 import 'screens/splash_screen.dart';
 
-void main() => runApp(
-      MaterialApp(
-        title: 'YogaNath',
-        home: SplashScreen(),
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/',
-        onGenerateRoute: RouteGenerator.generateRoute,
-        theme: ThemeData(
-          typography: Typography.material2018(platform: TargetPlatform.android),
-          primaryColor: Color(0xffcc9c74),
-          primaryColorDark: Color(0xff996E48),
-          buttonColor: Color(0xff5F9269),
-          accentColor: Color(0xff32643E),
-          scaffoldBackgroundColor: Colors.white,
-          textTheme: Typography.blackMountainView.copyWith(
-            bodyText2: TextStyle(color: Colors.black54, fontSize: 16.0),
-            headline5: TextStyle(
-              fontFamily: 'Indie Flower',
-              fontSize: 33.0,
-              color: Color(0xff5F9269),
-            ),
-            headline6: TextStyle(
-              fontFamily: 'Indie Flower',
-              fontSize: 27.0,
-              color: Colors.black87,
-            ),
+void main() => runApp(YogaNath());
+
+class YogaNath extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'YogaNath',
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
+      theme: ThemeData(
+        typography: Typography.material2018(platform: TargetPlatform.android),
+        primaryColor: Color(0xffcc9c74),
+        primaryColorDark: Color(0xff996E48),
+        buttonColor: Color(0xff5F9269),
+        accentColor: Color(0xff32643E),
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: Typography.blackMountainView.copyWith(
+          bodyText2: TextStyle(color: Colors.black54, fontSize: 16.0),
+          headline5: TextStyle(
+            fontFamily: 'Indie Flower',
+            fontSize: 33.0,
+            color: Color(0xff5F9269),
           ),
-          // appBarTheme: AppBarTheme(brightness: Brightness.dark),
+          headline6: TextStyle(
+            fontFamily: 'Indie Flower',
+            fontSize: 27.0,
+            color: Colors.black87,
+          ),
         ),
+        // appBarTheme: AppBarTheme(brightness: Brightness.dark),
       ),
     );
+  }
+}
