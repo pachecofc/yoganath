225a0348abd52dcdef9ef0db65377149274035e4
Fábio Luiz Camacho Pacheco <fabiopacheco@iMac-de-Fabio.local>
2020-07-27 12:17:22 -0300

    fix: render layout screens

--- lib/screens/schedule_screen.dart
+++ lib/screens/schedule_screen.dart
@@ -7,52 +7,54 @@ import 'package:yoganath/widgets/reusableTitle.dart';
 class Schedule extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ReusableTitle(text: 'Nath Agenda', isPremium: false),
-        Card(
-          shadowColor: Theme.of(context).canvasColor,
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Icon(
-                    Icons.calendar_today,
-                    color: Theme.of(context).buttonColor,
-                  ),
-                  Column(
-                    children: <Widget>[
-                      ReusableSubtitle(text: 'Eventos'),
-                      Text('Presenciais'),
-                    ],
-                  ),
-                  Icon(
-                    Icons.share,
-                    color: Theme.of(context).buttonColor,
-                  )
-                ],
-              ),
-              Image.asset('assets/images/mapa.png'),
-              Text(
-                  'Greyhound divisively hello coldly wonderfully marginally far…'),
-              ReusableFlatButton(
-                  buttonText: 'ADICIONAR AO CALENDÁRIO', onPressed: () {}),
-            ],
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ReusableTitle(text: 'Nath Agenda', isPremium: false),
+          Card(
+            shadowColor: Theme.of(context).canvasColor,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(
+                      Icons.calendar_today,
+                      color: Theme.of(context).buttonColor,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        ReusableSubtitle(text: 'Eventos'),
+                        Text('Presenciais'),
+                      ],
+                    ),
+                    Icon(
+                      Icons.share,
+                      color: Theme.of(context).buttonColor,
+                    )
+                  ],
+                ),
+                Image.asset('assets/images/mapa.png'),
+                Text(
+                    'Greyhound divisively hello coldly wonderfully marginally far…'),
+                ReusableFlatButton(
+                    buttonText: 'ADICIONAR AO CALENDÁRIO', onPressed: () {}),
+              ],
+            ),
           ),
-        ),
-        SizedBox(
-          height: 46.5,
-        ),
-        ReusableTitle(text: 'Aulas ao Vivo', isPremium: true),
-        ReusableCard(
-          cardImage: 'sirshasana.jpg',
-          cardText: 'Data: 01/ago/2020',
-          secondTextLine: 'Horário: 19:30',
-          reusableFlatButton:
-              ReusableFlatButton(buttonText: 'AGENDAR', onPressed: () {}),
-        ),
-      ],
+          SizedBox(
+            height: 46.5,
+          ),
+          ReusableTitle(text: 'Aulas ao Vivo', isPremium: true),
+          ReusableCard(
+            cardImage: 'sirshasana.jpg',
+            cardText: 'Data: 01/ago/2020',
+            secondTextLine: 'Horário: 19:30',
+            reusableFlatButton:
+                ReusableFlatButton(buttonText: 'AGENDAR', onPressed: () {}),
+          ),
+        ],
+      ),
     );
   }
 }
