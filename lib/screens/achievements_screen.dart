f4600e25ccccb02914743c25bc886c944ad5a2a1
Fábio Pacheco <fabiopacheco@iMac-de-Fabio.local>
2020-08-07 14:46:54 -0300

    refactor: make responsive to small devices

--- lib/screens/achievements_screen.dart
+++ lib/screens/achievements_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yoganath/widgets/reusableAchievement.dart';
+import 'package:yoganath/widgets/reusableFlatButton.dart';
+import 'package:yoganath/widgets/reusableYogaPoints.dart';
 
 class Achievements extends StatelessWidget {
   @override
@@ -7,6 +9,7 @@ class Achievements extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
+          ProfileHeader(),
           ReusableAchievement(
             goalTitle: 'Ásanas',
             goalDescription: 'Complete 14 aulas',
@@ -36,6 +39,57 @@ class Achievements extends StatelessWidget {
             goalDescription: 'Complete 30 leituras',
             progress: 90.0,
             numOfStars: 4,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileHeader extends StatelessWidget {
+  const ProfileHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 11.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // Avatar
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/meditation.png',
+                  width: 60.0,
+                  height: 68.09,
+                ),
+                ReusableFlatButton(
+                    buttonText: 'INSERIR FOTO', onPressed: () {}),
+              ],
+            ),
+          ),
+          // User name and level
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    'Nathalia Steindorfer',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Text('Mestre'),
+              ],
+            ),
+          ),
+          // User score
+          Expanded(
+            child: ReusableYogaPoints(score: 512),
           ),
         ],
       ),
