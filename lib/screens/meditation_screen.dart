aff35d75294ef567f9b8efd34b91c2d59957315b
Fábio Pacheco <fabiopacheco@iMac-de-Fabio.local>
2020-08-08 14:44:07 -0300

    fix: input times

--- lib/screens/meditation_screen.dart
+++ lib/screens/meditation_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:yoganath/services/routeGenerator.dart';
 import 'package:yoganath/widgets/reusableMidiaCard.dart';
 import 'package:yoganath/widgets/reusableRaisedButton.dart';
 import 'package:yoganath/widgets/reusableTextFormField.dart';
 
 class Meditation extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _hourController = TextEditingController();
+  final TextEditingController _minuteController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final Size _screenSize = MediaQuery.of(context).size;
@@ -19,7 +22,7 @@ class Meditation extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: SingleChildScrollView(
             child: Column(
-              children: <Widget>[
+              children: [
                 SizedBox(
                   height: _screenSize.height * 0.27,
                   width: _screenSize.width * 0.54,
@@ -27,16 +30,11 @@ class Meditation extends StatelessWidget {
                       imageName: 'meditating.jpg', isChecked: false),
                 ),
                 SizedBox(height: _screenSize.height * 0.07),
-                AdjustTimer(),
-                SizedBox(height: _screenSize.height * 0.07),
-                Sound(),
-                SizedBox(height: _screenSize.height * 0.07),
-                ReusableRaisedButton(
-                    buttonText: 'INICIAR',
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(
-                          context, RouteGenerator.kFEEDBACK_ROUTE);
-                    }),
+                ChooseTime(
+                    formKey: _formKey,
+                    screenSize: _screenSize,
+                    hourController: _hourController,
+                    minuteController: _minuteController),
                 ListTile(
                   leading: IconButton(
                     icon: Icon(
@@ -56,12 +54,80 @@ class Meditation extends StatelessWidget {
   }
 }
 
-class Sound extends StatefulWidget {
+class ChooseTime extends StatelessWidget {
+  const ChooseTime({
+    Key key,
+    @required GlobalKey<FormState> formKey,
+    @required Size screenSize,
+    @required TextEditingController hourController,
+    @required TextEditingController minuteController,
+  })  : _formKey = formKey,
+        _screenSize = screenSize,
+        _hourController = hourController,
+        _minuteController = minuteController,
+        super(key: key);
+
+  final GlobalKey<FormState> _formKey;
+  final Size _screenSize;
+  final TextEditingController _hourController;
+  final TextEditingController _minuteController;
+
   @override
-  _SoundState createState() => _SoundState();
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                width: _screenSize.width * 0.34,
+                child: ReusableTextFormField(
+                  textEditingController: _hourController,
+                  isObscure: false,
+                  label: 'Hora',
+                  hint: '0',
+                  errorMessage:
+                      'Por favor, digite um número maior ou igual a zero',
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              SizedBox(
+                width: _screenSize.width * 0.34,
+                child: ReusableTextFormField(
+                    textEditingController: _minuteController,
+                    isObscure: false,
+                    label: 'Minutos',
+                    hint: null,
+                    errorMessage:
+                        'Por favor, digite um número maior ou igual a zero',
+                    keyboardType: TextInputType.number),
+              ),
+            ],
+          ),
+          SizedBox(height: _screenSize.height * 0.07),
+          ChooseSound(),
+          SizedBox(height: _screenSize.height * 0.07),
+          ReusableRaisedButton(
+            buttonText: 'INICIAR',
+            onPressed: () {
+              Navigator.pushReplacementNamed(
+                  context, RouteGenerator.kFEEDBACK_ROUTE);
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _SoundState extends State<Sound> {
+class ChooseSound extends StatefulWidget {
+  @override
+  _ChooseSoundState createState() => _ChooseSoundState();
+}
+
+class _ChooseSoundState extends State<ChooseSound> {
   String _selectedSound = 'Escolha um som';
 
   @override
@@ -95,59 +161,6 @@ class _SoundState extends State<Sound> {
           _selectedSound = value;
         });
       },
-    );
-  }
-}
-
-class AdjustTimer extends StatelessWidget {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _hourController = TextEditingController();
-  final TextEditingController _minuteController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    final double _width = MediaQuery.of(context).size.width;
-
-    return Container(
-      // child: ReusableTextFormField(
-      //     textEditingController: _hourController,
-      //     isObscure: false,
-      //     label: 'Hora',
-      //     hint: null,
-      //     errorMessage: null,
-      //     keyboardType: TextInputType.number),
-      // TODO Fix input errors
-      child: Form(
-        key: _formKey,
-        autovalidate: true,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            SizedBox(
-              width: _width * 0.34,
-              child: ReusableTextFormField(
-                textEditingController: _hourController,
-                isObscure: false,
-                label: 'Hora',
-                hint: '0',
-                errorMessage: null,
-                keyboardType: TextInputType.number,
-              ),
-            ),
-            SizedBox(
-              width: _width * 0.34,
-              child: ReusableTextFormField(
-                textEditingController: _minuteController,
-                isObscure: false,
-                label: 'Minutos',
-                hint: '0',
-                errorMessage: 'Digite um valor entre zero e 59.',
-                keyboardType: TextInputType.number,
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
