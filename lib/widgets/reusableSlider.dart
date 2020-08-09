0c99f2f28f9db1aa675f76e7f75194fb95fa73b3
Fábio Pacheco <fabiopacheco@iMac-de-Fabio.local>
2020-08-09 08:19:19 -0300

    refactor: adaptive slider

--- lib/widgets/reusableSlider.dart
+++ lib/widgets/reusableSlider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ReusableSlider extends StatelessWidget {
@@ -7,13 +8,14 @@ class ReusableSlider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Slider(
+    return Slider.adaptive(
       value: initialValue,
       min: 0,
       max: 100,
       divisions: 9,
       label: '$initialValue%',
       onChanged: (double newValue) {},
+      activeColor: Theme.of(context).sliderTheme.activeTrackColor,
     );
   }
 }
